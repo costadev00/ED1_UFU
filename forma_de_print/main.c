90358c006739996adcc3a4e7f7fbe99d08d84c50
Matheus Costa <matheuscostamonteiro.mc@gmail.com>
2020-03-30 20:13:47 -0300

    Commit pelo terminal linux

---
+++ forma_de_print/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    float a,b,c;
+    scanf("%d",&i);
+    scanf("%f",&a);
+    scanf("%f",&b);
+    scanf("%f",&c);
+    if(a>b && a>c && b>c){
+        printf("%.2f\n",a);
+        printf("%.2f\n",b);
+        printf("%.2f\n",c);
+    }
+    if(a>b && a>c && c>b){
+        printf("%.2f\n",a);
+        printf("%.2f\n",c);
+        printf("%.2f\n",b);
+    }
+    if(b>a && b>c && a>c){
+        printf("%.2f\n",b);
+        printf("%.2f\n",a);
+        printf("%.2f\n",c);
+    }
+    if(b>a && b>c && c>b){
+        printf("%.2f\n",b);
+        printf("%.2f\n",c);
+        printf("%.2f\n",a);
+    }
+    if(c>a && c>b && a>b){
+        printf("%.2f\n",c);
+        printf("%.2f\n",a);
+        printf("%.2f\n",b);
+    }
+    if(c>a && c>b && a>b){
+        printf("%.2f\n",c);
+        printf("%.2f\n",b);
+        printf("%.2f\n",a);
+    }
+    return 0;
+}
