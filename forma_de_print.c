5034e2a01d294f40d6b9c31357b6111d36183ebd
Matheus Costa <matheuscostamonteiro.mc@gmail.com>
2020-05-05 02:58:56 -0300

    now it works

--- forma_de_print.c
+++ forma_de_print.c
@@ -4,40 +4,46 @@
 int main()
 {
     int i;
-    float a,b,c;
-    scanf("%d",&i);
-    scanf("%f",&a);
-    scanf("%f",&b);
-    scanf("%f",&c);
-    if(a>b && a>c && b>c){
-        printf("%.2f\n",a);
-        printf("%.2f\n",b);
-        printf("%.2f\n",c);
+    float a, b, c;
+    scanf("%d", &i);
+    scanf("%f", &a);
+    scanf("%f", &b);
+    scanf("%f", &c);
+    if (a > b && a > c && b > c)
+    {
+        printf("%.2f\n", a);
+        printf("%.2f\n", b);
+        printf("%.2f\n", c);
     }
-    if(a>b && a>c && c>b){
-        printf("%.2f\n",a);
-        printf("%.2f\n",c);
-        printf("%.2f\n",b);
+    if (a > b && a > c && c > b)
+    {
+        printf("%.2f\n", a);
+        printf("%.2f\n", c);
+        printf("%.2f\n", b);
     }
-    if(b>a && b>c && a>c){
-        printf("%.2f\n",b);
-        printf("%.2f\n",a);
-        printf("%.2f\n",c);
+    if (b > a && b > c && a > c)
+    {
+        printf("%.2f\n", b);
+        printf("%.2f\n", a);
+        printf("%.2f\n", c);
     }
-    if(b>a && b>c && c>b){
-        printf("%.2f\n",b);
-        printf("%.2f\n",c);
-        printf("%.2f\n",a);
+    if (b > a && b > c && c > b)
+    {
+        printf("%.2f\n", b);
+        printf("%.2f\n", c);
+        printf("%.2f\n", a);
     }
-    if(c>a && c>b && a>b){
-        printf("%.2f\n",c);
-        printf("%.2f\n",a);
-        printf("%.2f\n",b);
+    if (c > a && c > b && a > b)
+    {
+        printf("%.2f\n", c);
+        printf("%.2f\n", a);
+        printf("%.2f\n", b);
     }
-    if(c>a && c>b && a>b){
-        printf("%.2f\n",c);
-        printf("%.2f\n",b);
-        printf("%.2f\n",a);
+    if (c > a && c > b && a > b)
+    {
+        printf("%.2f\n", c);
+        printf("%.2f\n", b);
+        printf("%.2f\n", a);
     }
     return 0;
 }
