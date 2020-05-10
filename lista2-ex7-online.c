5d5f6191df2d0211dd976504c79985278fb2cca1
Matheus Costa <matheuscostamonteiro.mc@gmail.com>
2020-05-10 20:42:17 -0300

    now everything in list2 is done.

--- lista2-ex7-online.c
+++ lista2-ex7-online.c
@@ -6,6 +6,38 @@ armazene em uma terceira matriz e a mostre na tela.
 #include <stdlib.h>
 int main()
 {
+    int mat1[2][2], mat2[2][2], mat3[2][2];
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            scanf("%d", &mat1[i][j]);
+        }
+    }
 
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            scanf("%d", &mat2[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            mat3[i][j] = mat1[i][j] + mat2[i][j];
+        }
+    }
+    printf("\n");
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            printf("%d\t", mat3[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
--- lista2-ex8-online.c
+++ lista2-ex8-online.c
@@ -2,10 +2,34 @@
 8- Faça um programa que carregue uma matriz 3x3 com números inteiros, some os elementos de
 cada linha e armazene em um vetor, depois mostre os resultados.
 */
+/*
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 int main()
 {
+    int mat[3][3], vet[3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            vet[i] = mat[i][0] + mat[i][1] + mat[i][2];
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%d\n", vet[i]);
+    }
 
     return 0;
 }
