711491fb19942dc2b6adb7b113afa5c5ed5293d3
Matheus Costa <matheuscostamonteiro.mc@gmail.com>
2020-05-28 21:06:01 -0300

    done morse code!

--- CODIGO-MORSE.c
+++ CODIGO-MORSE.c
@@ -1,42 +1,110 @@
-/*
-
-*/
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-int main()
+#include <ctype.h>
+static const struct
 {
-    char frase[100], codigo[100];
-    scanf("%[^\n]", frase);
-    int i = 0;
-    do
+    const char letter, *morse;
+} Code[] =
     {
+        {'A', ".- "},
+        {'B', "-... "},
+        {'C', "-.-. "},
+        {'D', "-.. "},
+        {'E', ". "},
+        {'F', "..-. "},
+        {'G', "--. "},
+        {'H', ".... "},
+        {'I', ".. "},
+        {'J', ".--- "},
+        {'K', ".-.- "},
+        {'L', ".-.. "},
+        {'M', "-- "},
+        {'N', "-. "},
+        {'O', "--- "},
+        {'P', ".--. "},
+        {'Q', "--.- "},
+        {'R', ".-. "},
+        {'S', "... "},
+        {'T', "- "},
+        {'U', "..- "},
+        {'V', "...- "},
+        {'W', ".-- "},
+        {'X', "-..- "},
+        {'Y', "-.-- "},
+        {'Z', "--.. "},
+        {' ', "  "},
+};
+void codifica(const char *s)
+{
+    size_t i, j;
+    for (i = 0; s[i]; ++i)
+    {
+        for (j = 0; j < sizeof Code / sizeof *Code; ++j)
+        {
+            if (toupper(s[i]) == Code[j].letter)
+            {
+                printf("%s", Code[j].morse);
+                break;
+            }
+        }
+    }
+    putchar('\n');
+}
+void decodifica(const char *morse)
+{
+    printf("função decodifica\n");
+    size_t i, j;
+    for (i = 0; morse[i];)
+    {
+        for (j = 0; j < sizeof Code / sizeof *Code; ++j)
+        {
+            size_t size = strlen(Code[j].morse);
+            if (memcmp(Code[j].morse, &morse[i], size) == 0)
+            {
+                putchar(Code[j].letter);
+                i += size;
+                break;
+            }
+        }
+    }
+    putchar('\n');
+}
+int main(void)
+{
+    int op = 0;
+    char text[50];
+    char test[50];
+    printf("\t\t\t\tDigite sua opcao\n1_Codificar\n2_Decodificar\n:");
+    scanf("%d", &op);
+    switch (op)
+    {
+    case 1:
+    {
+        printf("Digite seu texto a ser codificado\n");
+        scanf(" %[^\n]", text);
+        codifica(text);
+        break;
+    }
+    case 2:
+    {
+        printf("Digite seu texto a ser decodificado\n");
+        scanf(" %[^\n]", test);
+        int len;
+        len = strlen(test);
+        if (test[len - 1] != ' ')
+        {
+            strcat(test, " "); //se falta space .. coloca um ;)
+        }
+        decodifica(test);
+        break;
+    }
+    default:
+    {
+        printf("Vc digitou uma opcao nao correspondente!!");
+        main();
+        break;
+    }
+    }
 
-        while (frase[i] == 'a' || frase[i] == 'A')
-        {
-            codigo[i] = '.';
-            i++;
-            codigo[i] = '-';
-            i++;
-        }
-        while (frase[i] == 'b' || frase[i] == 'B')
-        {
-            codigo[i] = '-';
-            i++;
-            codigo[i] = '.';
-            i++;
-            codigo[i] = '.';
-            i++;
-            codigo[i] = '.';
-            i++;
-        }
-        // if (frase[i] == " ")
-        // {
-        //     codigo[i] = ' ';
-        //     // i++;
-        // }
-        // frase[i] = (frase[i] + acm_frase);
-    } while (frase[i] != '\0');
-    printf("%s\n", codigo);
     return 0;
 }
