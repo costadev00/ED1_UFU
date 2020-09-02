dc4a9955feb069b5684258148b5b2d99dc9b1921
Matheus Costa Monteiro <43221564+costadev00@users.noreply.github.com>
2020-09-02 13:04:41 -0300

    last exercise befere stack class

---
+++ exercicio-string.c
@@ -0,0 +1,39 @@
+/*
+Fazer um programa que recebe 2 strings e quer saber se uma é permutação da outra. 
+Ou seja se tem as msm letras e a msm quantidade de letras... 
+Por exemplo se eu escrever amor em uma e Roma em outra, tem q dar q elas são iguais
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    char str1[100], str2[100];
+    int tam1, tam2, cont = 0;
+    gets(str1);
+    gets(str2);
+    tam1 = strlen(str1);
+    tam2 = strlen(str2);
+    if (tam1 == tam2)
+    {
+        for (int i = 0; i < tam1; i++)
+        {
+            for (int j = 0; j < tam2; j++)
+            {
+                if (str1[i] == str2[j])
+                {
+                    cont++;
+                }
+            }
+        }
+        if (cont == tam1)
+        {
+            printf("Palavras iguais e com as mesmas letras\n");
+        }
+        else
+        {
+            printf("Palvras diferentes\n");
+        }
+        return 0;
+    }
+}
